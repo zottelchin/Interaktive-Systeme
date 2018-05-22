f279585faa7e44f88927f3368371a335106972ee
Jones <tjisendahl@googlemail.com>
2018-05-22 10:27:30 +0200

    Python-Code, der nicht funktioniert :c

--- SohlenConnection.py
+++ SohlenConnection.py
@@ -4,9 +4,10 @@ class SohlenConnection:
 
     ser = serial.Serial(port='COM4',baudrate=11520,timeout=10)
     print(ser.isOpen())
-    values = bytearray([0x02])
-    print(values)
-    print("Write {} bytes".format(values))
+    values = bytearray([2])
+    print(ser.in_waiting)
+    print(ser.out_waiting)
+    ser.write(values)
     print(ser.in_waiting)
     print(ser.out_waiting)
     res = ser.read(10)
