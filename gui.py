d97669dbd21c69718880e023883147cae102ca5b
Emonator <nepomuk.markarian@st.ovgu.de>
2018-05-29 12:12:06 +0200

    Small changes

--- gui.py
+++ gui.py
@@ -1,28 +1,29 @@
-import tkinter 
+from tkinter import *
 
 def createUI() :
-    top = tkinter.Tk()
+    top = Tk()
 
-    testButton1 = tkinter.Button(top, text = "Hello")
+    testButton1 = Button(top, text = "Hello", activebackground = "green")
     testButton1.pack()
 
-    testListbox1 = tkinter.Listbox(top)
-    testListbox1.insert(1, "HEllo")
+    testListbox1 = Listbox(top)
+    testListbox1.insert(1, "Hello")
     testListbox1.insert(2, "Bye")
     testListbox1.pack()
 
-    testCheckBox1 = tkinter.Checkbutton(top, text = "Arr! This be a Checkbox")
-    testCheckBox2 = tkinter.Checkbutton(top, text = "Shiver me timbers!")
+    testCheckBox1 = Checkbutton(top, text = "Arr! This be a Checkbox")
+    testCheckBox2 = Checkbutton(top, text = "Shiver me timbers!")
     testCheckBox1.pack()
     testCheckBox2.pack()
 
-    var = tkinter.IntVar()
+    var = IntVar()
 
-    R1 = tkinter.Radiobutton(top, text="Option 1", variable=var, value=1)
-    R1.pack( anchor = tkinter.W )
-    R2 = tkinter.Radiobutton(top, text="Option 2", variable=var, value=2)
-    R2.pack( anchor = tkinter.W )
-    R3 = tkinter.Radiobutton(top, text="Option 3", variable=var, value=3)
-    R3.pack( anchor = tkinter.W)
+    R1 = Radiobutton(top, text="Option 1", variable=var, value=1)
+    R1.pack( anchor = W )
+    R2 = Radiobutton(top, text="Option 2", variable=var, value=2)
+    R2.pack( anchor = W )
+    R3 = Radiobutton(top, text="Option 3", variable=var, value=3)
+    R3.pack( anchor = W)
+
 
     top.mainloop()
