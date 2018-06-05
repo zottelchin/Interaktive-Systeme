fb83009429f5c5938199b74ee1626ba789eedda0
Unknown <zottelchin@gmail.com>
2018-06-05 09:44:59 +0200

    setoffset

--- Main.py
+++ Main.py
@@ -2,6 +2,18 @@ import Heatmap
 import SohlenConnection
 import Interpreter
 
+offsetRight = [[0 for x in range(5)]for y in range(10)]
+offsetLeft = [[0 for x in range(5)]for y in range(10)]
+
+def setOffset():
+    raw_left_data = SohlenConnection.getData("L")
+    left_data = Interpreter.inputRawDataLeft(raw_left_data)
+    offsetLeft = left_data
+
+    raw_right_data = SohlenConnection.getData("R")
+    right_data = Interpreter.inputRawDataRight(raw_right_data)
+    offsetRight = right_data
+
 run = True
 while run:
     #raw_left_data = SohlenConnection.getData("L")
